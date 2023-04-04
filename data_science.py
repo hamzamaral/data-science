47279438bf231c08a130fac32327187e46b8553b
computerahmetmortal <129831885+computerahmetmortal@users.noreply.github.com>
2023-04-04 10:12:14 +0300

    c2datasine
    c2datasine

--- data_science.py
+++ data_science.py
@@ -6,17 +6,17 @@ Created on Wed Jun 1 00:18:36 2022
 """
 
 
-# This Python 3 environment comes with many helpful analytics libraries installed
-# It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-python
-# For example, here's several helpful packages to load in 
+# This Python 3 environment comes with many helpful analytics libraries installedfcfc
+# It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-pythonfcfc
+# For example, here's several helpful packages to load in fcddexqdecfcfcfc
 
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import matplotlib.pyplot as plt
 import seaborn as sns  # visualization tool
 
-# Input data files are available in the "../input/" directory.
-# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
+#dechvgdhbcdofbhhbdkxlömnjcefd2dc2decıuhdecıudeıcdecıunfıuencufencıufecfffffffffffffffffffff
+# For examplecefcfc, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
 
 from subprocess import check_output
 print(check_output(["ls", "../input"]).decode("utf8"))
